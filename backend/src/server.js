984e294767aeda477b00c7b5a64674838df9505b
ghaly26 <eec.ghali2012@gmail.com>
2026-09-04 16:41:10 -0500

    Fix: Re-routed Resend endpoint to verified mail delivery-{resend}

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,7 +1,7 @@
 import express from 'express';
 import cors from 'cors';
 import axios from 'axios';
-import resend from 'resend';
+import { resend } from 'resend';
 import dotenv from 'dotenv';
 dotenv.config();
 
