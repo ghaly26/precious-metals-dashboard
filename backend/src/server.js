7b83e95097ce773fd969b4272618b8a4a86039d1
ghaly.youssef <eec.ghali2012@gmail.com>
2026-09-12 16:29:48 -0500

    Fix:PayloadTooLargeError for PDF invoices email

--- backend/src/server.js
+++ backend/src/server.js
@@ -8,7 +8,10 @@ dotenv.config();
 const app = express();
 app.set('trust proxy', true);
 app.use(cors());
-app.use(express.json());
+// app.use(express.json());
+// Replace your old app.use(express.json()) with this:
+app.use(express.json({ limit: '50mb' }));
+app.use(express.urlencoded({ limit: '50mb', extended: true }));
 
 const troyOunceToGram = 31.1035;
 
